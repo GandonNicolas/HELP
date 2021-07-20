4a4301251bc791f63dd3dfe4ab30162eb9314b85
h <hesham99100@gmail.com>
2021-07-20 12:03:58 +0200

    création de PostController et affichage de page d'ajout d'article avec twig

---
+++ src/Controller/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/post", name="post")
+     */
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+      /**
+     * @Route("/post/add", name="post_add")
+     */
+    public function add(): Response
+    {
+        return $this->render('post/add.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+}
+
