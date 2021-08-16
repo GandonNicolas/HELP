5b21ba7446d6cca4bb6fe38bfbbc9a5d2427c256
h <hesham99100@gmail.com>
2021-08-16 13:19:27 +0200

    last changes

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
@@ -36,6 +37,49 @@ class PostController extends AbstractController
             'controller_name' => 'PostController',
         ]);
     }
+    -----
     
+     /**
+     * @Route("/add", name="post_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $Post = new Post();
+        $form = $this->createForm(PostType::class, $Post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($Post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_index');
+        }
+
+        return $this->render('post/add.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/modify", name="post_modify", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Post $post): Response
+    {
+        $form = $this->createForm(KeywordType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('keyword_index');
+        }
+
+        return $this->render('post/modify.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
 }
 
